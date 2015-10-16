5e5f12cd5b3d080eb22962d7a9e060995c5d4e40
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2015-10-15 21:36:56 -0700

    Add .gitignore

--- iphone/build/ti.googlemaps.build/Release-iphonesimulator/ti.googlemaps.build/Objects-normal/i386/DeHansknoechelGooglemapsModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hans/Documents/appelerator_modules/ti.googlemaps/iphone/Classes/DeHansknoechelGooglemapsModuleAssets.m \
-  /Users/hans/Documents/appelerator_modules/ti.googlemaps/iphone/Classes/DeHansknoechelGooglemapsModuleAssets.h
