6ce4ccf6a5875ce57b26baa24c6601effbe43e73
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-01-15 18:47:31 +0100

    Fix formatting of all methods
    # Conflicts:
    #	ios/Classes/TiGooglemapsAnnotationProxy.m
    #	ios/Classes/TiGooglemapsCircleProxy.m
    #	ios/Classes/TiGooglemapsMapView.m
    #	ios/Classes/TiGooglemapsMapViewProxy.h
    #	ios/Classes/TiGooglemapsMapViewProxy.m
    #	ios/Classes/TiGooglemapsMarkerProxy.m
    #	ios/Classes/TiGooglemapsPolygonProxy.m
    #	ios/Classes/TiGooglemapsPolylineProxy.m
    #	ios/Classes/TiGooglemapsView.m
    #	ios/Classes/TiGooglemapsViewProxy.h
    #	ios/Classes/TiGooglemapsViewProxy.m

--- ios/Classes/TiGooglemapsMarkerProxy.h
+++ ios/Classes/TiGooglemapsMarkerProxy.h
@@ -17,7 +17,7 @@
  *  @param value The title
  *  @since 1.0.0
  */
--(void)setTitle:(id)value;
+- (void)setTitle:(id)value;
 
 /**
  *  Sets the annotation snippet.
@@ -26,7 +26,7 @@
  *  @deprecated 2.2.0
  *  @since 1.0.0
  */
--(void)setSnippet:(id)value __attribute((deprecated("Use subtitle instead.")));
+- (void)setSnippet:(id)value __attribute((deprecated("Use subtitle instead.")));
 
 /**
  *  Sets the annotation subtitle.
@@ -34,7 +34,7 @@
  *  @param value The subtitle
  *  @since 2.2.0
  */
--(void)setSubtitle:(id)value;
+- (void)setSubtitle:(id)value;
 
 /**
  *  Sets the annotation info window offset.
@@ -43,7 +43,7 @@
  *  @deprecated 2.2.0
  *  @since 1.0.0
  */
--(void)setInfoWindowAnchor:(id)args __attribute((deprecated("Use centerOffset instead.")));
+- (void)setInfoWindowAnchor:(id)args __attribute((deprecated("Use centerOffset instead.")));
 
 /**
  *  Sets the annotation center offset.
@@ -51,7 +51,7 @@
  *  @param args The point
  *  @since 2.2.0
  */
--(void)setCenterOffset:(id)args;
+- (void)setCenterOffset:(id)args;
 
 /**
  *  Sets the annotation ground offset.
@@ -59,7 +59,7 @@
  *  @param args The point
  *  @since 2.2.0
  */
--(void)setGroundOffset:(id)args;
+- (void)setGroundOffset:(id)args;
 
 /**
  *  Sets the annotation icon.
@@ -68,7 +68,7 @@
  *  @deprecated 2.2.0
  *  @since 1.0.0
  */
--(void)setIcon:(id)value __attribute((deprecated("Use image instead.")));
+- (void)setIcon:(id)value __attribute((deprecated("Use image instead.")));
 
 /**
  *  Sets the annotation image.
@@ -76,7 +76,7 @@
  *  @param value The image
  *  @since 2.1.0
  */
--(void)setImage:(id)value;
+- (void)setImage:(id)value;
 
 /**
  *  Sets the annotation pin color.
@@ -84,7 +84,7 @@
  *  @param value The color
  *  @since 2.1.0
  */
--(void)setPinColor:(id)value;
+- (void)setPinColor:(id)value;
 
 /**
  *  Sets the annotation touch-capacity.
@@ -93,7 +93,7 @@
  *  @deprecated 2.2.0
  *  @since 1.0.0
  */
--(void)setTappable:(id)value __attribute((deprecated("Use touchEnabled instead.")));;
+- (void)setTappable:(id)value __attribute((deprecated("Use touchEnabled instead.")));;
 
 /**
  *  Sets the annotation touch-capacity.
@@ -101,7 +101,7 @@
  *  @param value The boolean capacity
  *  @since 2.2.0
  */
--(void)setTouchEnabled:(id)value;
+- (void)setTouchEnabled:(id)value;
 
 /**
  *  Sets the annotation flat-capacity.
@@ -109,7 +109,7 @@
  *  @param value The boolean capacity
  *  @since 1.0.0
  */
--(void)setFlat:(id)value;
+- (void)setFlat:(id)value;
 
 /**
  *  Sets the annotation draggable-capacity.
@@ -117,7 +117,7 @@
  *  @param value The boolean capacity
  *  @since 1.0.0
  */
--(void)setDraggable:(id)value;
+- (void)setDraggable:(id)value;
 
 /**
  *  Sets the annotation opacity.
@@ -125,7 +125,7 @@
  *  @param value The boolean opacity
  *  @since 2.2.0
  */
--(void)setOpacity:(id)value;
+- (void)setOpacity:(id)value;
 
 /**
  *  Sets the annotation animation style.
@@ -133,7 +133,7 @@
  *  @param value The animation style
  *  @since 2.2.0
  */
--(void)setAnimationStyle:(id)value;
+- (void)setAnimationStyle:(id)value;
 
 /**
  *  Sets the annotation user data.
@@ -141,6 +141,6 @@
  *  @param value The user data
  *  @since 1.0.0
  */
--(void)setUserData:(id)value;
+- (void)setUserData:(id)value;
 
 @end
