0a2243f7c4fab367a5aea2bc7b8013bdddf8f4c0
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-01-15 19:07:40 +0100

    Force lat/lon before other props, remove setMarker

--- ios/Classes/TiGooglemapsMarkerProxy.m
+++ ios/Classes/TiGooglemapsMarkerProxy.m
@@ -28,16 +28,6 @@ NSLog(@"[WARN] Ti.GoogleMaps: %@ is deprecated since %@ in favor of %@", from, i
     return _marker;
 }
 
--(void)setMarker:(GMSMarker*)marker
-{
-    if (_marker) {
-        RELEASE_TO_NIL(_marker);
-    }
-    
-    _marker = marker;
-    [self marker];
-}
-
 -(void)dealloc
 {
     RELEASE_TO_NIL(_marker);
