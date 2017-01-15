3ab0c57839b92de7fe789d0be55f760ebf36ebad
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-01-15 18:59:51 +0100

    Revert "Remove unused "setMarker(s)" methods"
    This reverts commit 1d6c4339d928631c9c502adfadd39012c841cadd.
    
    # Conflicts:
    #	ios/Classes/TiGooglemapsAnnotationProxy.m
    #	ios/Classes/TiGooglemapsMarkerProxy.m

--- ios/Classes/TiGooglemapsViewProxy.m
+++ ios/Classes/TiGooglemapsViewProxy.m
@@ -58,6 +58,13 @@ NSLog(@"[WARN] Ti.GoogleMaps: %@ is deprecated since %@ in favor of %@", from, t
     return overlays;
 }
 
+-(void)setMarkers:(NSMutableArray*)_markers
+{
+    [markers removeAllObjects];
+    RELEASE_TO_NIL(markers);
+    markers = [[_markers mutableCopy] retain];
+}
+
 #pragma mark Public API's
 
 - (void)setUserInteractionEnabled:(id)value
